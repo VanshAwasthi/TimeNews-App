f3a45e3cdd5ccf22bb065d9aafe3acbffe0073de
Vansh Awasthi <75345433+VanshAwasthi@users.noreply.github.com>
2023-04-30 12:40:13 +0530

    ADD: basic structure for news item created

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title , description} = this.props;//yha destructuring horhi hai this.props ek object hai aur usme se title aur discription pull karke la rhe hai
     return (
       <div>
-        This is item 1
+        <div className="card" style={{width: "18rem"}}>
+            <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
       </div>
     )
   }
