b7ed888a4d9e2473fbe912d9e5803258785d1f0c
Vansh Awasthi <75345433+VanshAwasthi@users.noreply.github.com>
2023-04-30 20:52:51 +0530

    ADD: Looping

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+  
   render() {
-    let {title , description} = this.props;//yha destructuring horhi hai this.props ek object hai aur usme se title aur discription pull karke la rhe hai
+    let {title , description, imageUrl ,newsUrl} = this.props;//yha destructuring horhi hai this.props ek object hai aur usme se title aur discription pull karke la rhe hai
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
-            <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" className="card-img-top" alt="..."/>
+            <img src={imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}</p>
-                <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+                <p className="card-text">{description}...</p>
+                <a href={newsUrl} target="__blank" className="btn btn-sm btn-primary">Read More</a>
   </div>
 </div>
       </div>
