7465f51a686f0e2ccd77526b6d1cb486dbc115b7
Vansh Awasthi <75345433+VanshAwasthi@users.noreply.github.com>
2023-05-02 01:33:43 +0530

    ADD: Next and Previous button

--- src/components/News.js
+++ src/components/News.js
@@ -2,56 +2,60 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-    articles=[
-        {
-        "source": {
-        "id": "espn-cric-info",
-        "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-        "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-        "publishedAt": "2020-04-27T11:41:47Z",
-        "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-        "source": {
-        "id": "espn-cric-info",
-        "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-        "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-        "publishedAt": "2020-03-30T15:26:05Z",
-        "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-        ]
 
 // props pass karke aap state ko set kar skte ho aur fir state ko change kar skte ho lekin props ko change nhi kar skte ho(props are only read only)
 // state is used when you change variable again and again without loading the page
     constructor(){
         super();
-        console.log("Hello I am constructor from the News Component");
         this.state = {
-            articles: this.articles,//aricles variable jo upar banaya hai usko access kar skte hai this.article karke
-            loading: false
+            articles: [],
+            loading: false,
+            page:1
+
         }
+    }
+    // componentDidMount runs after render method runs ,in this case used to update all news articles
+    async componentDidMount(){
+      let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=d5a0ea757b884e8bb0bfebe698fc1d07&page=1";
+      let data = await fetch(url);// fetch api takes url and return promise
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({articles: parsedData.articles})//properties cannot be null we have to deal with it
+    }
+     handlePrevClick = async ()=>{
+        console.log("Prev")
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d5a0ea757b884e8bb0bfebe698fc1d07&page=${this.state.page - 1}`;
+        let data = await fetch(url);// fetch api takes url and return promise
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({articles: parsedData.articles,
+          page: this.state.page - 1})
+      }
+      
+      handleNextClick = async ()=>{
+      console.log("Next")
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d5a0ea757b884e8bb0bfebe698fc1d07&page=${this.state.page + 1}`;
+      let data = await fetch(url);// fetch api takes url and return promise
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({articles: parsedData.articles,
+        page: this.state.page + 1})
     }
   render() {
       //to iterate use map and map return something here it is returing div so unique id=key is in div
     return (
       <div className='container my-3 '>
-        <h2>TimeNews - Top Headline</h2>
+        <h1>TimeNews - Top Headline</h1>
         <div className="row">
         {this.state.articles.map((element)=>{
            return <div className="col-md-4" key={element.url}> 
-                <NewsItem title={element.title.slice(0,45)} description={element.description.slice(0,88)} imageUrl={element.urlToImage} newsUrl={element.url} />
+                <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url} />
             </div>
         })}
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick} >&larr; Previous</button>
+        <button type="button" className="btn btn-dark" onClick={this.handleNextClick} >Next &rarr;</button>
         </div>
       </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+            <img src={!imageUrl?"https://images.hindustantimes.com/tech/img/2023/04/30/1600x900/asteroid-4369511_1920_Pixabay_1682824505795_1682824517645.jpg":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}...</p>
-                <a href={newsUrl} target="__blank" className="btn btn-sm btn-primary">Read More</a>
+                <p className="card-text">{description}</p>
+                <a href={newsUrl} target="__blank" className="btn btn-sm btn-dark">Read More</a>
   </div>
 </div>
       </div>
