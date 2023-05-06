bbb3a77dac4527419b970ffac3d9f54681d360d7
Vansh Awasthi <75345433+VanshAwasthi@users.noreply.github.com>
2023-05-06 21:50:00 +0530

    class base to functionl based al comp excpt App.js

--- src/components/News.js
+++ src/components/News.js
@@ -1,154 +1,136 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-export class News extends Component {
-  static deafaultProps = {
-    country: "in",
-    pageSize: 8,
-    category: "general",
-  };
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
-  };
-  capitalizeFirstLetter = (string) => {
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState([true]);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+  // document.title = `${capitalizeFirstLetter(
+  //   props.category
+  // )} - TimeNews`;
+
+  const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
   // props pass karke aap state ko set kar skte ho aur fir state ko change kar skte ho lekin props ko change nhi kar skte ho(props are only read only)
   // state is used when you change variable again and again without loading the page
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      loading: true,
-      page: 1,
-      totalResults: 0,
-    };
-    document.title = `${this.capitalizeFirstLetter(
-      this.props.category
-    )} - TimeNews`;
-  }
-  //Code refactoring ki hai isleye code commented hai handlePrevClick aur handleNextClick ka
-  async updateNews() {
-    this.props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
+
+  const updateNews = async () => {
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
     let data = await fetch(url); // fetch api takes url and return promise
-    this.props.setProgress(30);
+    props.setProgress(30);
     let parsedData = await data.json();
-    this.props.setProgress(70);
-    // console.log(parsedData);
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-      loading: false,
-    }); //properties cannot be null we have to deal with it
-    this.props.setProgress(100);
-  }
-  // componentDidMount runs after render method runs ,in this case used to update all news articles
-  async componentDidMount() {
-    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d5a0ea757b884e8bb0bfebe698fc1d07&page=1&pageSize=${this.props.pageSize}`;
-    // this.setState({loading: true});
-    // let data = await fetch(url);// fetch api takes url and return promise
-    // let parsedData = await data.json();
-    // console.log(parsedData);
-    // this.setState({articles: parsedData.articles,
-    //   totalResults:parsedData.totalResults,
-    //   loading: false})//properties cannot be null we have to deal with it
-    this.updateNews();
-  }
-  handlePrevClick = async () => {
-    // console.log("Prev")
-    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d5a0ea757b884e8bb0bfebe698fc1d07&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-    // this.setState({loading: true});
-    // let data = await fetch(url);// fetch api takes url and return promise
-    // let parsedData = await data.json();
-    // this.setState({articles: parsedData.articles,
-    //   page: this.state.page - 1,
-    //   loading: false})
-    this.setState({ page: this.state.page - 1 });
-    this.updateNews(); //this. isleye kyo ki ham class ke andar hai
+    props.setProgress(70);
+
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+
+    //properties cannot be null we have to deal with it
+    props.setProgress(100);
   };
 
-  handleNextClick = async () => {
-    // console.log("Next")
-    // if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
+  useEffect(() => {
+    updateNews();
+  }, []);
 
-    //       let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d5a0ea757b884e8bb0bfebe698fc1d07&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-    //       this.setState({loading: true});
-    //       let data = await fetch(url);// fetch api takes url and return promise
-    //       let parsedData = await data.json();
-    //       this.setState({page: this.state.page + 1,
-    //         articles: parsedData.articles,
-    //         loading: false
-    //         })
-    // }
-    this.setState({ page: this.state.page + 1 });
-    this.updateNews(); //this. isleye kyo ki ham class ke andar hai
-  };
+  // const handlePrevClick = async () => {
+  //   // console.log("Prev")
+  //   // let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=d5a0ea757b884e8bb0bfebe698fc1d07&page=${this.state.page - 1}&pageSize=${props.pageSize}`;
+  //   // this.setState({loading: true});
+  //   // let data = await fetch(url);// fetch api takes url and return promise
+  //   // let parsedData = await data.json();
+  //   // this.setState({articles: parsedData.articles,
+  //   //   page: this.state.page - 1,
+  //   //   loading: false})
+  //   setPage(page-1)
+  //   updateNews(); //this. isleye kyo ki ham class ke andar hai
+  // };
+
+  // const handleNextClick = async () => {
+  //   // console.log("Next")
+  //   // if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/props.pageSize))){
+
+  //   //       let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=d5a0ea757b884e8bb0bfebe698fc1d07&page=${this.state.page + 1}&pageSize=${props.pageSize}`;
+  //   //       this.setState({loading: true});
+  //   //       let data = await fetch(url);// fetch api takes url and return promise
+  //   //       let parsedData = await data.json();
+  //   //       this.setState({page: this.state.page + 1,
+  //   //         articles: parsedData.articles,
+  //   //         loading: false
+  //   //         })
+  //   // }
+  //   setPage(page+1)
+  //   updateNews(); //this. isleye kyo ki ham class ke andar hai
+  // };
 
   //fetchMoreData is for infinite loading
-  fetchMoreData = async () => {
-    this.setState({ page: this.state.page + 1 });
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+  const fetchMoreData = async () => {
+    setPage(page + 1);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
     let data = await fetch(url); // fetch api takes url and return promise
     let parsedData = await data.json();
     // console.log(parsedData);
-    this.setState({
-      articles: this.state.articles.concat(parsedData.articles),
-      totalResults: parsedData.totalResults,
-    });
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults);
   };
-  render() {
-    //to iterate use map and map return something here it is returing div so unique id=key is in div
-    return (
-      <>
-        <div className="text-center" style={{ margin: "35px 0px" }}>
-          <h1>
-            TimeNews - Top {this.capitalizeFirstLetter(this.props.category)}{" "}
-            Headlines
-          </h1>
-        </div>
-        {this.state.loading && <Spinner />}
-        <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length !== this.state.totalResults}
-          loader={<Spinner />}
-        >
-          <div className="container">
-            <div className="row">
-              {this.state.articles.map((element) => {
-                return (
-                  <div className="col-md-4" key={element.url}>
-                    <NewsItem
-                      title={element.title ? element.title : ""}
-                      description={
-                        element.description ? element.description : ""
-                      }
-                      imageUrl={element.urlToImage}
-                      newsUrl={element.url}
-                      author={element.author ? element.author : "Unknown"}
-                      date={element.publishedAt}
-                      source={element.source.name}
-                    />
-                  </div>
-                );
-              })}
-            </div>
+  //to iterate use map and map return something here it is returing div so unique id=key is in div
+  return (
+    <>
+      <div className="text-center" style={{ margin: "35px 0px" }}>
+        <h1>
+          TimeNews - Top {capitalizeFirstLetter(props.category)} Headlines
+        </h1>
+      </div>
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
+          <div className="row">
+            {articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title : ""}
+                    description={element.description ? element.description : ""}
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                    author={element.author ? element.author : "Unknown"}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })}
           </div>
-        </InfiniteScroll>
-        {/* <div className="container d-flex justify-content-between">
+        </div>
+      </InfiniteScroll>
+      {/* <div className="container d-flex justify-content-between">
         <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick} >&larr; Previous</button>
-        <button  disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick} >Next &rarr;</button>
+        <button  disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick} >Next &rarr;</button>
         </div> */}
-      </>
-    );
-  }
-}
+    </>
+  );
+};
 
+News.deafaultProps = {
+  country: "in",
+  pageSize: 8,
+  category: "general",
+};
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
 export default News;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,48 +1,45 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date, source } =
-      this.props; //yha destructuring horhi hai this.props ek object hai aur usme se title aur discription pull karke la rhe hai
-    return (
-      <div className="my-3">
-        <div className="card">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              position: "absolute",
-              right: "0",
-            }}
-          >
-            <span className="badge rounded-pill bg-danger">{source}</span>
-          </div>
-          <img
-            src={
-              !imageUrl
-                ? "https://images.hindustantimes.com/tech/img/2023/04/30/1600x900/asteroid-4369511_1920_Pixabay_1682824505795_1682824517645.jpg"
-                : imageUrl
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <p className="card-text">
-              <small className="text-body-secondary">
-                By {author} on {new Date(date).toGMTString()}{" "}
-              </small>
-            </p>
-            {/*new date object banaya Date aur fir .toGMTString kardiya usko*/}
-            <a href={newsUrl} target="__blank" className="btn btn-sm btn-dark">
-              Read More
-            </a>
-          </div>
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props; //yha destructuring horhi hai this.props ek object hai aur usme se title aur discription pull karke la rhe hai
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span className="badge rounded-pill bg-danger">{source}</span>
+        </div>
+        <img
+          src={
+            !imageUrl
+              ? "https://images.hindustantimes.com/tech/img/2023/04/30/1600x900/asteroid-4369511_1920_Pixabay_1682824505795_1682824517645.jpg"
+              : imageUrl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              By {author} on {new Date(date).toGMTString()}{" "}
+            </small>
+          </p>
+          {/*new date object banaya Date aur fir .toGMTString kardiya usko*/}
+          <a href={newsUrl} target="__blank" className="btn btn-sm btn-dark">
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewsItem;
